99944524f7c93db20d31da4c46099d59c82e2675
Andrew S Erwin <andrewerwin73@gmail.com>
2024-07-09 10:58:58 -0500

    initial upload to github

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'ANDREW THE CODER';
+export const SITE_DESCRIPTION = 'personal website of Andrew S Erwin';
